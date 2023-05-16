9012711cb5aee112c7ebeefcbf8d8ad0c7c79950
Diele Correa <dybs1520@gmail.com>
2023-05-16 11:23:33 -0300

    Exercícios aula 16/05
    Fiz somente exercícios 1 e 2

---
+++ Exercicios.py
@@ -0,0 +1,27 @@
+#Ex1: Calcular a área de terreno (largura x altura)
+largura = float(input("Diga qual é largura do terreno?: "))
+altura = float(input("Diga qual é altura do terreno?: "))
+resultado=float
+resultado = largura * altura
+
+print("A area do seu terreno é: ", resultado)
+
+#Ex2: perguntar quantos pães franceses e quantos sonhos o cliente deseja comprar. PÃO = 0,50 R$ e SONHO = 2,00 R$. (lembre-se de exibir o valor final.)
+pao = float
+sonho = float
+valorTotal = float
+valorpao= float
+valorsonho = float
+
+pao = 0.50
+sonho = 2.0
+
+
+qntpao = float(input("quantos pães você quer? "))
+valorpao = qntpao * pao
+qntsonho = float(input("quantos sonhos você quer? "))
+valorsonho = qntsonho * sonho
+
+valorTotal = valorpao + valorsonho
+
+print("valor total da sua compra é: ", valorTotal)
