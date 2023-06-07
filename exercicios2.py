fc5d88f734ce64612e4a8068820dd16d49026837
Diele Correa <135260061+Henriquelz26@users.noreply.github.com>
2023-06-07 09:27:57 -0300

    Exercicio 1 da Lista dois usando .format

---
+++ exercicios2.py
@@ -0,0 +1,26 @@
+##exercicio 1#
+
+#n1 = float(input("Insira o Dividendo: "))
+#n2 = float(input("Insira o Divisor: "))
+
+#r = n1 / n2
+
+#if (r!=0):
+   #print("O resultado da divisão é: {} ".format(r))
+#else:
+ #   print("O numero é inválido")
+
+
+
+
+num = float(input("Diga um número: " ))
+divisao = float(input("Dividido por: " ))
+
+
+resultado = float 
+resultado = num / divisao
+
+if (resultado != 0):
+    print("O resultado da divisãoi é: {}".format(resultado))
+else:
+     print("O resultado é inválido : ")
