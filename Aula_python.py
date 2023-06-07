8830b9776fcd3bde6a8ac8bd37fab3eb0b68e5d5
Diele Correa <135260061+Henriquelz26@users.noreply.github.com>
2023-06-07 09:39:05 -0300

    Update Aula_python.py

--- Aula_python.py
+++ Aula_python.py
@@ -1,7 +1,7 @@
-x = 200
-y = 220
-contador = 0
-while x < y:
-    x=x+1
-    contador=contador+1
-print("O comando while rodou {} vezes" .format(contador))
+#x = 200
+#y = 220
+#contador = 0
+#while x < y:
+ #   x=x+1
+  #  contador=contador+1
+#print("O comando while rodou {} vezes" .format(contador))
