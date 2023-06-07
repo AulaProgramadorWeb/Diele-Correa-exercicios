33ac965bc72f2a74d0b3b31a96e1d8d9cf36c2a5
Diele Correa <dybs1520@gmail.com>
2023-06-07 10:23:12 -0300

    Incluindo exercício 3 da lista dois
    Ex 03.
    
    Fazer um programa onde o usuário vai digitar o valor atual do Dólar e
    Euro, Escolher qual opção de conversão ele deseja, e o valor para
    converter. Opções disponíveis: Euro para Dólar, Euro para Real, Dólar
    para Euro, Dólar para Real, Real para Euro e Real para Dólar. No final,
    perguntar para o usuário se ele deseja reiniciar o programa (se a
    resposta for sim, deve retornar ao início do programa).

--- exercicios2.py
+++ exercicios2.py
@@ -25,3 +25,23 @@ if (resultado >= 318.20):
     print("O valor máximo do desconto permitido é R$ 318,20")
 else:
    print("O valor do desconto é: R$ {} ".format(resultado))
+
+#Ex 03: Fazer um programa onde o usuário vai digitar o valor atual do Dólar e
+#Euro, Escolher qual opção de conversão ele deseja, e o valor para
+#converter. Opções disponíveis: Euro para Dólar, Euro para Real, Dólar
+#para Euro, Dólar para Real, Real para Euro e Real para Dólar. No final,
+#perguntar para o usuário se ele deseja reiniciar o programa (se a
+#resposta for sim, deve retornar ao início do programa)
+
+euro = float(input("Qual o valor atual do EURO: "))
+dolar = float(input("Qual o valor atual do DOLAR: "))
+real = float(input("Qual o valor atual do REAL: "))
+convert = float(input("Qual o valor para converter: "))
+
+euro = convert * euro
+dolar = convert * dolar
+real = convert * real
+
+print("valor em EURO: {}".format(euro), 
+      "valor em DOLAR: {}".format(dolar), 
+      "valor em REAL:{} ".format(real))
