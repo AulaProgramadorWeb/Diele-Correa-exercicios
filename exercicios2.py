18fdc26352207e59678927b78419b123e34678a8
Diele Correa <135260061+Henriquelz26@users.noreply.github.com>
2023-06-07 10:09:32 -0300

    incluindo exercício 2 da segunda lista
    #Ex 2: Calcular o desconto do salário de um funcionário (11%),
    #exibindo o valor ou o desconto máximo permitido (318,20R$)

--- exercicios2.py
+++ exercicios2.py
@@ -1,17 +1,5 @@
-##exercicio 1#
-
-#n1 = float(input("Insira o Dividendo: "))
-#n2 = float(input("Insira o Divisor: "))
-
-#r = n1 / n2
-
-#if (r!=0):
-   #print("O resultado da divisão é: {} ".format(r))
-#else:
- #   print("O numero é inválido")
-
-
-
+#Ex 1: Fazer uma calculadora de divisão com apenas 2 números, e
+#testar se o número é válido (não é zero)
 
 num = float(input("Diga um número: " ))
 divisao = float(input("Dividido por: " ))
@@ -24,3 +12,16 @@ if (resultado != 0):
     print("O resultado da divisãoi é: {}".format(resultado))
 else:
      print("O resultado é inválido : ")
+
+#Ex 2: Calcular o desconto do salário de um funcionário (11%),
+#exibindo o valor ou o desconto máximo permitido (318,20R$)
+
+salario = float(input("Qual o salario do funcionario: "))
+desconto = float(input("Qual a porcentagem de desconto:"))
+desconto = desconto / 100
+resultado = salario * desconto
+
+if (resultado >= 318.20):
+    print("O valor máximo do desconto permitido é R$ 318,20")
+else:
+   print("O valor do desconto é: R$ {} ".format(resultado))
